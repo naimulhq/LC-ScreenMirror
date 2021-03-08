170f1800fb00a7e0dce443115bb524f699c7dc1c
naimulhq <naimul@ucsb.edu>
2021-03-08 13:44:35 -0800

    Basic Screen Mirror through  OpenCV

--- GetScreen.py
+++ GetScreen.py
@@ -1,11 +1,16 @@
 import pyautogui
 import cv2
+import PIL
+import numpy as np
 
 def getScreenshot():
+    # Takes a screenshot
     while True:
         screenshot = pyautogui.screenshot()
-        screenshot.save(r'Screen.png')
-        cv2.imshow('frame',cv2.imread('Screen.png',-1))
+        screenshot = np.array(screenshot)
+        screenshot = cv2.cvtColor(screenshot,cv2.COLOR_BGR2RGB)
+        # Output image
+        cv2.imshow('frame',screenshot)
         if(cv2.waitKey(1) & 0xFF == ord('q')):
             break
     cv2.destroyAllWindows()

f6404c01f4d66396e61e24eaddc843323428b1db
Naimul Hoque <55862319+naimulhq@users.noreply.github.com>
2021-03-10 22:00:27 -0800

    NH - Server produces screenshot i.e Screen Mirror

--- AirplayServer.py
+++ AirplayServer.py
@@ -1,10 +1,12 @@
 import socket
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+import pickle
 
 # Create a socket that will recieve data
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    host = "127.0.0.1"
+    host = "192.168.1.120"
     port = 1800
     s.bind((host,port))
     s.listen(1)
@@ -13,9 +15,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print('Connected by', addr)
         data = b''
         while True:
-            part = conn.recv(40000,socket.MSG_WAITALL)
-            break
+            part = conn.recv(6220962,socket.MSG_WAITALL)
+            part = pickle.loads(part)
+            conn.send(bytes("Recieved","utf-8"))
+            cv2.imshow('frame',part)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-print("Bytes in Server: ", len(part))
-decoded = cv2.imdecode(np.frombuffer(part, np.uint8), -1)
-cv2.imwrite('PythonTest2.png',decoded)
+
+

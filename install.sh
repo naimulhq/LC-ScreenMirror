a4e8b4129378da0de74435bb2959713653997c24
Naimul Hoque <55862319+naimulhq@users.noreply.github.com>
2021-04-16 20:19:26 -0700

    NH - Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get install -y python3-pip python3-tk python3-pil python3-pil.imagetk
+sudo apt-get install -y python3-pip python3-tk python3-pil python3-pil.imagetk scrot
 pip3 install pyautogui numpy opencv-python
